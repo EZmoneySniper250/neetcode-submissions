6f7c190b45f8607b8abca825f812d7483a43b12d
Jerry Chen <99642279+EZmoneySniper250@users.noreply.github.com>
2026-08-17 22:11:13 -0400

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,28 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # #brute force is to sort all the two strings and compare the string one by one, this will be O(nlogn * n^2) complexity, since the python sort + the nested loop)
+        # #1. using the dictionary to count the occurances, similar to collections.Counter
+        # if len(s)!= len(t):
+        #     return False
+        # ans = defaultdict(int)
+        # for s_l in s:
+        #     ans[s_l]+=1
+        # for t_l in t:
+        #     if t_l not in t:
+        #         return False
+        #     ans[t_l]-=1
+        # return all(ans[x]==0 for x in ans.keys())
+        # #The dict method will actually have a time complexity of (O(2n)) and a space complexity of O(n), since there will be a defaultdict created in this case according to the length of the two strings. So I prpose a more efficient way to do this question, by the internal function ord() in python, which returns the ASCII number for each number, here's how it goes
+
+        if len(s)!=len(t):
+            return False
+        
+        cnt = [0]*26 #26 letters, we got O(1) since the length is fixed here, and that's the only list we create here
+        for i in range(len(s)):
+            #the only loop here --> O(n) in time
+            cnt[ord(s[i])-ord('a')] +=1
+            cnt[ord(t[i])-ord('a')] -=1
+            #if same letter then it's 0 otherwise values exist
+        return all(cnt[j] ==0 for j in range(26))
+
